f34e4354eeaa530436df8378c82d3a5ea07e3b80
COD3VIL <73862378+michaelnji@users.noreply.github.com>
2025-04-30 01:33:12 +0100

    style: 💄 Update UI elements in Navbar and Articles page, refine background gradient in custom CSS, and enhance Prop Firms page layout with new content and styling improvements.

---
+++ components/TutorialsList.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div>
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center gap-4">
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+            <TutorialCard />
+
+        </div>
+        <!-- <div class="mt-12 mx-auto max-w-xs w-full">
+            <button class=" btn btn-primary btn-block btn-lg   ">View all articles
+                <Icon name="ph:arrow-right" />
+            </button>
+        </div> -->
+    </div>
+</template>
