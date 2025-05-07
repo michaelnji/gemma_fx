66cfb244ae7da17f855359fad1ac1b5bedc7d38c
COD3VIL <73862378+michaelnji@users.noreply.github.com>
2025-05-07 23:28:00 +0100

    fix: 🐛 Correct variable name from 'proccessedPosts' to 'processedPosts' and enhance accessibility by adding aria-labels to NuxtLink components in related.vue.

--- components/related.vue
+++ components/related.vue
@@ -8,15 +8,11 @@
     const isLoading = ref(true)
     const posts = ref<Post[] | undefined>()
 
-    const proccessedPosts = computed(() => {
+    const processedPosts = computed(() => {
         const data = posts.value
         if (data) {
             return data.filter((x) => x.slug.current !== props.slug).filter((x) => {
-                let hasSimilar = false
-                x.tags.forEach((y) => {
-                    if (props.tags.find((tag) => y.title === tag.title)) { hasSimilar = true }
-                })
-                return hasSimilar
+                return x.tags.some((tag) => props.tags.some((propTag) => tag.title === propTag.title))
             }).filter((x, y) => y < 2)
         }
         return null
@@ -45,10 +41,11 @@
 <template>
     <div class="mt-12">
         <div v-if="isLoading" class="w-full  h-48 skeleton bg-base-300 lg:bg-base-200"></div>
-        <div class="" v-if="!isLoading && proccessedPosts && proccessedPosts.length > 0">
+        <div class="" v-if="!isLoading && processedPosts && processedPosts.length > 0">
             <h2 class="font-diaplay text-3xl font-bold">You might also like</h2>
             <div class="grid md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2  gap-4 mt-6">
-                <NuxtLink v-for="post in proccessedPosts" :to="post.slug.current">
+                <NuxtLink v-for="post in processedPosts" :to="post.slug.current"
+                    :aria-label="`Read article: ${post.title}`">
                     <div class=" pl-3 border-l-2 border-primary">
 
                         <div class="">
@@ -61,10 +58,11 @@
                 </NuxtLink>
             </div>
         </div>
-        <div class="" v-if="!isLoading && proccessedPosts && proccessedPosts.length <= 0 && nextPost">
+        <div class="" v-if="!isLoading && processedPosts && processedPosts.length <= 0 && nextPost">
             <h2 class="font-diaplay text-3xl font-bold">Next article</h2>
             <div class="grid md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2  gap-4 mt-6">
-                <NuxtLink :to="`/articles/${nextPost.slug.current}`">
+                <NuxtLink :aria-label="`Read next article: ${nextPost.title}`"
+                    :to="`/articles/${nextPost.slug.current}`">
                     <div class=" pl-3 border-l-2 border-primary">
 
                         <div class="">
